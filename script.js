26d6d7090f2fb1284489dd1c72c92d139912fc90
Nurselam <77880259+WNurselam@users.noreply.github.com>
2022-05-13 11:29:44 +0300

    readm-update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+//Form inputlarını id ile tek tek alıyoruz
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const repassword = document.getElementById('repassword');
 
+//Forma kayıt olduğunda hata var ise error mesajı verme
 function error(input, message) {
     input.className = 'form-control is-invalid';
     const div = input.nextElementSibling;
@@ -15,6 +17,7 @@ function success(input) {
     input.className = 'form-control is-valid';
 }
 
+//İnputların değerini regex ile kontrol ediyoruz.
 function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
    
@@ -25,6 +28,7 @@ function checkEmail(input) {
     }
 }
 
+//İnputların değerlerinin girilmesinin zorunlu hale getiriyoruz
 function checkRequired(inputs) {
     inputs.forEach(function(input) {
         if(input.value === '') {
@@ -35,6 +39,7 @@ function checkRequired(inputs) {
     });  
 }
 
+// İnputlara girilen değerlerin minumun ve maximum ne kadar olmasını belirliyoruz
 function checkLength(input, min, max) {
     if (input.value.length < min) {
         error(input, `${input.id} en az ${min} karakter olmalıdır`);
@@ -45,18 +50,21 @@ function checkLength(input, min, max) {
     }
 }
 
+//Şifre ve Şifreyi tekrar gir inputların değerlerinin eşit olmaması durumunda eşleşmiyor mesajı verme
 function checkPasswords(input1,input2) {
     if(input1.value !== input2.value) {
         error(input2, 'Parolalar eşleşmiyor');
     }
 }
 
+// Telefon karakterinin regex ile number olma durumunu kontrol ediyoruz
 function checkPhone(input) {
     var exp = /^\d{10}$/;   
     if(!exp.test(input.value)) 
         error(input, 'Telefon 10 karakterli olmalıdır.');
 }
 
+// furma tıklandığında belirlediğimiz fonksiyonları çalıştırmaya başlıyoruz.
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
